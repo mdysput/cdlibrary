5e1ed2842c3bde35f0ba2e31fc675b2e7d1bd184
dominisz <dominisz@gmail.com>
2018-04-05 20:22:54 +0200

    App class

--- src/main/java/pl/dominisz/cdlibrary/App.java
+++ src/main/java/pl/dominisz/cdlibrary/App.java
@@ -1,5 +1,7 @@
 package pl.dominisz.cdlibrary;
 
+import java.util.Scanner;
+
 /**
  * http://dominisz.pl
  * 25.03.2018
@@ -7,6 +9,7 @@ package pl.dominisz.cdlibrary;
 public class App {
 
     private CDLibrary cdLibrary = new CDLibrary();
+    private Scanner scanner = new Scanner(System.in);
 
     public void showMainMenu() {
         cdLibrary.loadFromFile();
@@ -15,10 +18,25 @@ public class App {
             System.out.println("1. Add new CD");
             System.out.println("2. Show all CDs");
             System.out.println("3. Exit");
-
-
+            int option = scanner.nextInt();
+            switch (option) {
+                case 1:
+                    addNewCD();
+                    break;
+                case 2:
+                    showAllCDs();
+                    break;
+            }
         }
         cdLibrary.saveToFile();
+    }
+
+    private void showAllCDs() {
+
+    }
+
+    private void addNewCD() {
+
     }
 
     public static void main(String[] args) {
