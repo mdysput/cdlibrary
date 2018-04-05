92450012580efd1672a8670c3ef363e14063ceea
dominisz <dominisz@gmail.com>
2018-04-05 17:38:32 +0200

    Initial commit

---
+++ src/test/java/pl/dominisz/cdlibrary/AppTest.java
@@ -0,0 +1,19 @@
+package pl.dominisz.cdlibrary;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * http://dominisz.pl
+ * 25.03.2018
+ */
+class AppTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void main() {
+    }
+}
