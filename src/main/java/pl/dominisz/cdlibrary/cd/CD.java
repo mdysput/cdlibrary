f28658691efa84d2fa087c624dd3f4626569a3e9
dominisz <dominisz@gmail.com>
2018-04-10 20:04:24 +0200

    CD

--- src/main/java/pl/dominisz/cdlibrary/cd/CD.java
+++ src/main/java/pl/dominisz/cdlibrary/cd/CD.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * http://dominisz.pl
@@ -59,6 +60,8 @@ public class CD {
     }
 
     public Set<Genre> getGenres() {
-        return new HashSet<>();
+        return tracks.stream()
+                .flatMap(track -> track.getGenres().stream())
+                .collect(Collectors.toSet());
     }
 }
